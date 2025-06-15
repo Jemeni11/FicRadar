81892adb5f2e4fdf1ccbe655f494f1a023673322
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-15 22:47:52 +0100

    feat: add new utils to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,16 @@
+import copyToClipboard from "./copyToClipboard"
 import customError from "./customError"
 import delay from "./delay"
+import extractUsername from "./extractUsername"
 import getPaginatedSearchUrl from "./getPaginatedSearchUrl"
 import sortByCountDescending from "./sortByCountDescending"
 import withDomain from "./withDomain"
 
 export {
+  copyToClipboard,
   customError,
   delay,
+  extractUsername,
   getPaginatedSearchUrl,
   sortByCountDescending,
   withDomain
