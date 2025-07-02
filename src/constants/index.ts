a89a40441912a177d3849eb3ef0a28545a5ebc19
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-03 00:11:16 +0100

    chore(constants): extend SUPPORTED_SITES with forum.* domains

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,8 @@ import type { SupportedSites } from "@/types"
 
 export const SUPPORTED_SITES: Record<string, SupportedSites> = {
   "forum.questionablequesting.com": "QuestionableQuesting",
+  "forum.spacebattles.com": "SpaceBattles",
   "forums.spacebattles.com": "SpaceBattles",
+  "forum.sufficientvelocity.com": "SufficientVelocity",
   "forums.sufficientvelocity.com": "SufficientVelocity",
 }
