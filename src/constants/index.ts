0863519f810b99f047cfea517e58f226e794cb4c
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-02 21:48:15 +0100

    feat(constants): add new constant: Supported Sites

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import type { SupportedSites } from "@/types"
+
+export const SUPPORTED_SITES: Record<string, SupportedSites> = {
+  "forum.questionablequesting.com": "QuestionableQuesting",
+  "forums.spacebattles.com": "SpaceBattles",
+  "forums.sufficientvelocity.com": "SufficientVelocity",
+}
