894d00a70732b123cea5192ff2260cafabad04a0
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-02 21:47:00 +0100

    chore(utils): export isValidURL from index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ import customError from "./customError"
 import delay from "./delay"
 import extractUsername from "./extractUsername"
 import getPaginatedSearchUrl from "./getPaginatedSearchUrl"
+import isValidURL from "./isValidURL"
 import sortByCountDescending from "./sortByCountDescending"
 import withDomain from "./withDomain"
 
@@ -12,6 +13,7 @@ export {
   delay,
   extractUsername,
   getPaginatedSearchUrl,
+  isValidURL,
   sortByCountDescending,
-  withDomain
+  withDomain,
 }
