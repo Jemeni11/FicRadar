cffc8a29aefbba3c1921b5ff98306dc46dfb7927
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-02 21:36:36 +0100

    refactor(sufficientvelocity): Changed storyObject to StoryResult for consistency

--- src/adapters/sufficientvelocity.ts
+++ src/adapters/sufficientvelocity.ts
@@ -1,11 +1,11 @@
-import type { ProgressData, storyObject } from "@/types"
+import type { ProgressData, StoryResult } from "@/types"
 
 import getXenForoData from "./xenforo"
 
 async function getSufficientVelocityData(
   userURL: string,
-  progressCallback?: (progress: ProgressData) => void
-): Promise<storyObject[]> {
+  progressCallback?: (progress: ProgressData) => void,
+): Promise<StoryResult[]> {
   const adapterName = "SufficientVelocityAdapter"
   const baseURL = "https://forums.sufficientvelocity.com/"
 
