84128e32eeacc210d0916fffedd4b487dc2d9be4
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2026-03-03 15:42:18 +0100

    fix(extractUsername): handle /search/member URLs and update asset paths
    - Change favicon URLs from ../../assets/icon.png to ~assets/icon.png for both author-scrape.html and file-upload.html
    - Add guard for /members/ + exclude /search/ to avoid incorrect matches

--- src/utils/extractUsername.ts
+++ src/utils/extractUsername.ts
@@ -1,8 +1,15 @@
 export default function extractUsername(url: string): string | undefined {
   try {
-    const path = new URL(url).pathname // "/members/abcdef.12345/"
-    const match = path.match(/\/members\/([^./]+)/)
-    return match?.[1] // "abcdef"
+    if (url.includes("/members/") && !url.includes("/search/")) {
+      const path = new URL(url).pathname // "/members/abcdef.12345/"
+      const match = path.match(/\/members\/([^./]+)/)
+      return match?.[1] // "abcdef"
+    } else {
+      // https://forum.questionablequesting.com/search/member?user_id=
+      const search = new URL(url).search // "/members/abcdef.12345/"
+      const value = `User ${search.slice(1).split("=")[1]}`
+      return value
+    }
   } catch {
     return undefined
   }
